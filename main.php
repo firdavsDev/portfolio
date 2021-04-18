951d1f285c24126b4c565c667e4ffbaade830a79
Davronbek Boltayev <74819987+firdavsDev@users.noreply.github.com>
2021-04-19 03:35:36 +0500

    uzgardi yana

--- main.php
+++ main.php
@@ -1,23 +1,19 @@
 <?php
 
-
-
 $name=$_POST['name'];
 $email=$_POST['email'];
 $mavzu=$_POST['mav'];
 $message=$_POST['message'];
 $to='xackercoder@gmail.com';
 
-
+$headers="From: ".$name."\r\n" ."CC: hamimkabir@hamimkabir.webhostingindia.racing";
 
 $txt="Sizning protofoliyayizdan xabar keldi shu insondan ".$name ."\r\nEmail ".$email."\r\nXabar: ". $message;
 
 if($email!=NULL){
-    mail($to,$mavzu,$txt);
+    mail($to,$mavzu,$txt,$headers);
 }
 
 header('Location:inndex.html');
-
-
 
 ?>
