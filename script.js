93524cae621269d7f242aae2d8c6bdbc2164d9b1
Firdavs <Xackercoder@gmail.com>
2021-08-26 04:37:51 +0500

    add

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Firdavs", "Backend","Pythonist"],
+        strings: ["Djano", "Backend","Pythonist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Firdavs", "Backend", "Pythonist"],
+        strings: ["Djano", "Backend", "Pythonist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
