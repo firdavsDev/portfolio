d8899c1eb0b0ee42d86550d84367fa00847ac5e8
Davronbek Boltayev <74819987+firdavsDev@users.noreply.github.com>
2021-09-18 16:51:06 +0500

    Python

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Django", "Backend","Pythoness"],
+        strings: ["Django", "RestAPI","Python"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Django", "Backend", "Pythoness"],
+        strings: ["Django", "RestAPI", "Python"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
